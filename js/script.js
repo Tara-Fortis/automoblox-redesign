6a0a8dd8acb9301723738ba0d88c317033cb6f2f
Tineil_A <tine6573@gmail.com>
2025-12-02 19:51:55 -0500

    Added Bootstrap, header and footer files, created carousel with placeholder images

--- js/script.js
+++ js/script.js
@@ -1,2 +1,25 @@
-// current date
-document.getElementById("year").innerHTML = new Date().getFullYear();
+// load header and footer on each page
+function loadHTML(id, file) {
+    fetch(file)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Failed to load " + file);
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        })
+        .catch(error => console.error(error));
+}
+document.addEventListener("DOMContentLoaded", () => {
+    loadHTML("footer", "partials/footer.html");
+    loadHTML("header", "partials/header.html");
+});
+
+// carousel
+const myCarouselElement = document.querySelector('#carouselAutoPlay')
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 2000,
+    touch: false
+});
