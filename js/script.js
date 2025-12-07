1d9077d434976cbf7a71d8f2172a95d2e8a824e3
Tineil_A <tine6573@gmail.com>
2025-12-07 13:16:26 -0500

    debugged the js active nav links to work on github pages

--- js/script.js
+++ js/script.js
@@ -21,9 +21,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // active navigation links
 function setActiveNavLink() {
-    const currentPath = window.location.pathname;
+    const currentPath = window.location.pathname.split("/").pop();
     document.querySelectorAll(".nav-link").forEach(link => {
-        if (link.getAttribute("href") === currentPath) {
+        const linkPath = link.getAttribute("href").split("/").pop();
+        if (linkPath === currentPath) {
             link.classList.add("active");
         } else {
             link.classList.remove("active");
